55a8962eb9e738d5e802e8d23b22eee6373b4e78
Michael Chan <michaelcychan@gmail.com>
2022-07-27 18:14:28 +0100

    first move - first test passed

--- game.js
+++ game.js
@@ -1,6 +1,23 @@
 class Game {
+  constructor(playerO, playerX, randomiser) {
+    this.playerO = playerO;
+    this.playerX = playerX;
+    this.randomiser = randomiser;
+    this.whoseTurn = this.whoFirst();
+    this.board = [[null, null, null], [null, null, null], [null, null, null]]
+  }
   showBoard() {
-    return [[null, null, null], [null, null, null], [null, null, null]];
+    return this.board;
+  }
+  getWhoseTurn() {
+    return this.whoseTurn;
+  }
+  whoFirst(){
+    return (this.randomiser.pickPlayer() >= 0.5) ? this.playerO : this.playerX;
+  }
+  place(x, y){
+    (this.whoseTurn === this.playerO) ? this.board[x][y] = "O" : this.board[x][y] = "X";
+    (this.whoseTurn === this.playerO) ? this.whoseTurn = this.playerX : this.whoseTurn = this.playerO
   }
 }
 
--- tests/game.spec.js
+++ tests/game.spec.js
@@ -3,11 +3,38 @@ const Game = require('../game');
 describe('Game', () => {
   describe('initialisation', () => {
     it('initialises with an empty board', () => {
-      const player1Double = {};
-      const player2Double = {};
-      const game = new Game(player1Double, player2Double);
+      const playerODouble = {};
+      const playerXDouble = {};
+      const randomiserDouble = {pickPlayer: () => 0.5};
+      const game = new Game(playerODouble, playerXDouble, randomiserDouble);
       expect(game.showBoard()).toStrictEqual([[null, null, null], [null, null, null], [null, null, null]]);
-    })
-    
+    });
+    it('lets player 1 to be the first player for randomiser give a value equal or more than 0.5', () => {
+      const playerODouble = {};
+      const playerXDouble = {};
+      const randomiserDouble = {pickPlayer: () => 0.5};
+      const game = new Game(playerODouble, playerXDouble, randomiserDouble);
+      expect(game.getWhoseTurn()).toBe(playerODouble);
+    });
+    it('lets player 2 to be the first player for randomiser give a value less than 0.5', () => {
+      const playerODouble = {};
+      const playerXDouble = {};
+      const randomiserDouble = {pickPlayer: () => 0.49};
+      const game = new Game(playerODouble, playerXDouble, randomiserDouble);
+      expect(game.getWhoseTurn()).toBe(playerXDouble);
+    });
   });
+  describe('first move', () => {
+    it('shows the board and next player correctly when player O places at 1,1', () => {
+      const playerODouble = {place: () => (1,1)};
+      const playerXDouble = {};
+      const randomiserDouble = {pickPlayer: () => 0.5};
+      const game = new Game(playerODouble, playerXDouble, randomiserDouble);
+      game.place(0,0);
+      expect(game.showBoard()).toStrictEqual([["O", null, null], [null, null, null], [null, null, null]]);
+      expect(game.getWhoseTurn()).toBe(playerXDouble);
+    });
+    
+
+  })
 })
